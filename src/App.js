52d9283a57a2e41c85aa9c2f5d0597faf29e8b3b
Janco Steenkamp <jancosteenkamp3@gmail.com>
2022-04-29 12:41:37 +0200

    p132

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import './App.css';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -63,8 +65,10 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
   }
 
   const [stories, dispatchStories] = React.useReducer(
@@ -72,22 +76,22 @@ const App = () => {
     { data: [], isLoading: false, isError: false}
   );
 
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-  
-    fetch(url) // B
-      .then(response => response.json()) // C
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits, // D
-        });
-      })
-      .catch(() =>
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
+
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    }
   }, [url]);
+
 
   React.useEffect(() => {
     handleFetchStories();
@@ -116,26 +120,14 @@ const App = () => {
   
 
   return (
-    <div>
-      <h1>My hacker stories.</h1>
+    <div className="container">
+      <h1 className="headline-primary">My hacker stories.</h1>
       
-      <InputWithLabel
-        value={searchTerm}
-        id="search"
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Search
-      </button>
-
-      <hr />
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />        
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -148,6 +140,31 @@ const App = () => {
     </div>
   );
 };
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit
+}) => (
+  <form className="search-form" onSubmit={onSearchSubmit}>
+        <InputWithLabel
+          value={searchTerm}
+          id="search"
+          isFocused
+          onInputChange={onSearchInput}
+        >
+          <strong>Search:</strong>
+        </InputWithLabel>
+
+      <button
+        className="button button_large"
+        type="submit"
+        disabled={!searchTerm}
+        >
+          Search
+        </button>
+      </form>
+);
 
 const InputWithLabel = ({
   id,
@@ -168,7 +185,7 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children} </label>
+      <label htmlFor={id} className="label">{children} </label>
       &nbsp;
       <input
         ref={inputRef}
@@ -177,6 +194,7 @@ const InputWithLabel = ({
         value={value}
         autoFocus={isFocused}
         onChange={onInputChange}
+        className="input"
       />
     </>
   );
@@ -192,15 +210,15 @@ const List = ({ list, onRemoveItem }) =>
   );
 
 const Item = ({ item, onRemoveItem }) => (
-  <div>
-    <span>
+  <div className="item">
+    <span style={{width: '40%'}}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-        <button type="button" onClick={()=> onRemoveItem(item)}>
+    <span style={{width: '30%'}}>{item.author}</span>
+    <span style={{width: '10%'}}>{item.num_comments}</span>
+    <span style={{width: '10%'}}>{item.points}</span>
+    <span style={{width: '10%'}}>
+        <button className="button button_small" type="button" onClick={()=> onRemoveItem(item)}>
           Dismiss          
       </button>
     </span>
