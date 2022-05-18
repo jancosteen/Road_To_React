9dd1bb1894d38e5320ee849d9facb64408f7971e
Janco Steenkamp <jancosteenkamp3@gmail.com>
2022-05-18 12:52:22 +0200

    p144

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import './App.css';
+import styled from 'styled-components';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -48,6 +48,88 @@ const storiesReducer = (state, action) => {
       throw new Error();
   }
 };
+
+/************** Styled-Components ************************/
+
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: white-space;
+  text-overflow: ellipses;
+
+  a{
+    color: inherit;
+  }
+
+  width: ${props => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding:  5px;
+  cursor: pointer;
+
+  transition: all 0.1s  ease-in;
+
+  &:hover {
+    background: #171212;
+    color:  #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding:  5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding:  10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding:  10px  0 20px  0;
+  display:  flex;
+  align-items:  baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px; 
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom:  1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
+
+/*********************************************************/
 
 
 const App = () => {
@@ -108,20 +190,20 @@ const App = () => {
     });
   };
 
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-  };   
+  // const handleSearch = (event) => {
+  //   setSearchTerm(event.target.value);
+  // };   
   
-  const searchedStories = stories.data.filter(story =>
-    story.title
-      .toLowerCase()
-      .includes(searchTerm.toLowerCase())
-  );
+  // const searchedStories = stories.data.filter(story =>
+  //   story.title
+  //     .toLowerCase()
+  //     .includes(searchTerm.toLowerCase())
+  // );
   
 
   return (
-    <div className="container">
-      <h1 className="headline-primary">My hacker stories.</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My hacker stories.</StyledHeadlinePrimary>
       
       <SearchForm
         searchTerm={searchTerm}
@@ -137,7 +219,7 @@ const App = () => {
         <List list={stories.data} onRemoveItem={handleRemoveStory}/>  
       )}     
 
-    </div>
+    </StyledContainer>
   );
 };
 
@@ -146,7 +228,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit
 }) => (
-  <form className="search-form" onSubmit={onSearchSubmit}>
+  <StyledSearchForm onSubmit={onSearchSubmit}>
         <InputWithLabel
           value={searchTerm}
           id="search"
@@ -156,14 +238,13 @@ const SearchForm = ({
           <strong>Search:</strong>
         </InputWithLabel>
 
-      <button
-        className="button button_large"
+      <StyledButtonLarge
         type="submit"
         disabled={!searchTerm}
         >
           Search
-        </button>
-      </form>
+        </StyledButtonLarge>
+      </StyledSearchForm>
 );
 
 const InputWithLabel = ({
@@ -185,16 +266,15 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id} className="label">{children} </label>
+      <StyledLabel htmlFor={id}>{children} </StyledLabel>
       &nbsp;
-      <input
+      <StyledInput
         ref={inputRef}
         id={id}
         type={type}
         value={value}
         autoFocus={isFocused}
         onChange={onInputChange}
-        className="input"
       />
     </>
   );
@@ -210,19 +290,21 @@ const List = ({ list, onRemoveItem }) =>
   );
 
 const Item = ({ item, onRemoveItem }) => (
-  <div className="item">
-    <span style={{width: '40%'}}>
+  <StyledItem>
+    <StyledColumn width= "40%">
       <a href={item.url}>{item.title}</a>
-    </span>
-    <span style={{width: '30%'}}>{item.author}</span>
-    <span style={{width: '10%'}}>{item.num_comments}</span>
-    <span style={{width: '10%'}}>{item.points}</span>
-    <span style={{width: '10%'}}>
-        <button className="button button_small" type="button" onClick={()=> onRemoveItem(item)}>
+    </StyledColumn>
+    <StyledColumn width= "30%">{item.author}</StyledColumn>
+    <StyledColumn width= "10%">{item.num_comments}</StyledColumn>
+    <StyledColumn width= "10%">{item.points}</StyledColumn>
+    <StyledColumn width= "10%">
+      <StyledButtonSmall
+        type="button"
+        onClick={() => onRemoveItem(item)}>
           Dismiss          
-      </button>
-    </span>
-  </div>
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
 );
 
 export default App;
